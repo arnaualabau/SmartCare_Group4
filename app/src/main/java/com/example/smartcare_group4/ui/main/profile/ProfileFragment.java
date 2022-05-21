82168a305a4fbf8cae14bdf224c39e127ca72056
mariacasals <48765531+mariacasals@users.noreply.github.com>
2022-05-21 18:25:57 +0200

    menu lateral updated

--- app/src/main/java/com/example/smartcare_group4/ui/main/profile/ProfileFragment.java
+++ app/src/main/java/com/example/smartcare_group4/ui/main/profile/ProfileFragment.java
@@ -1,4 +1,4 @@
-package com.example.smartcare_group4.ui.main.gallery;
+package com.example.smartcare_group4.ui.main.profile;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,23 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.smartcare_group4.databinding.FragmentGalleryBinding;
+import com.example.smartcare_group4.databinding.FragmentProfileBinding;
 
-public class GalleryFragment extends Fragment {
 
-    private FragmentGalleryBinding binding;
+public class ProfileFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        GalleryViewModel galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
+        ProfileViewModel profileViewModel =
+                new ViewModelProvider(this).get(ProfileViewModel.class);
 
-        binding = FragmentGalleryBinding.inflate(inflater, container, false);
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textProfile;
+        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
