1e63a1f453ec4d0ff48db9a5dc49beea1b3b3ce7
mariacasals <48765531+mariacasals@users.noreply.github.com>
2022-06-13 11:01:01 +0200

    home neteja i control errors ok

--- app/src/main/java/com/example/smartcare_group4/ui/main/home/HomeViewModel.java
+++ app/src/main/java/com/example/smartcare_group4/ui/main/home/HomeViewModel.java
@@ -10,17 +10,9 @@ import com.example.smartcare_group4.data.repository.FirebaseRepository;
 
 public class HomeViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
-
     public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment. Jo aqui posaria tres botons: sensors, planning i emergencia.");
-
     }
 
-    public LiveData<String> getText() {
-        return mText;
-    }
 
     public MutableLiveData<String> setValuesEmergency() {
         MutableLiveData<String> data = new MutableLiveData<String>();
