9966fa4fc5fe65277138c3fd78723da5b247a67a
mariacasals <48765531+mariacasals@users.noreply.github.com>
2022-06-13 10:52:45 +0200

    signup neteja i control errors ok

--- app/src/main/java/com/example/smartcare_group4/ui/init/signup/SignupViewModel.java
+++ app/src/main/java/com/example/smartcare_group4/ui/init/signup/SignupViewModel.java
@@ -8,16 +8,7 @@ import com.example.smartcare_group4.data.repository.FirebaseRepository;
 
 public class SignupViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
-
-    public SignupViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is signup fragment");
-    }
-
-    public LiveData<String> getText() {
-        return mText;
-    }
+    public SignupViewModel() {}
 
     public LiveData<String> signUp(String email, String password) {
 
@@ -29,7 +20,6 @@ public class SignupViewModel extends ViewModel {
 
     public LiveData<String> registerUser(String name, String email, String id, String hardwareId, boolean patient) {
 
-        //MutableLiveData<String> data = (MutableLiveData<String>) firebase.registerUser(name, email, password, id, hardwareId, patient);
         MutableLiveData<String> data = (MutableLiveData<String>) FirebaseRepository.firebaseInstance.registerUser(name, email, id, hardwareId, patient);
 
         return data;
@@ -44,7 +34,7 @@ public class SignupViewModel extends ViewModel {
     }*/
 
     public boolean checkPSW(String psw1, String psw2) {
-
+        //Firebase password requirements
         if(psw1.equals(psw2)) {
             if (psw1.length() < 6) {
                 return false;
@@ -69,7 +59,6 @@ public class SignupViewModel extends ViewModel {
         if (s1.equals("")) {
             return true;
         }
-
         return false;
     }
 }
